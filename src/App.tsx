6b3c896f223e1e8d949766737c0ceda8e841a393
Ernest T Javachava <ernestjavaz@gmail.com>
2026-01-02 14:11:17 +0200

    initial set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <>
+    <div className="flex justify-center " > <h1 className="text-6xl text-amber-500"> hello</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
